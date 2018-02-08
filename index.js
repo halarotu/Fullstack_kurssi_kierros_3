e58ea075e91a2d601d196b51e18788490bab0f14
halarotu <hannes.alarotu@gmail.com>
2018-02-08 21:50:58 +0200

    3.1-3.2

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Martti Tienari",
+        "number": "040-123456",
+        "id": 2
+      },
+      {
+        "name": "Arto Järvinen",
+        "number": "040-123456",
+        "id": 3
+      },
+      {
+        "name": "Lea Kutvonen",
+        "number": "040-123456",
+        "id": 4
+      }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    const count = persons.length
+    res.send(`<div><p>Puhelinluettelossa on ${count} henkilön tiedot</p> <p>${date}</p></div>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
