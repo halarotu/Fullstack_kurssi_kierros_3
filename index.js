bcce7972c5cfd63169aa18418f52422f770bce07
halarotu <hannes.alarotu@gmail.com>
2018-02-08 23:15:20 +0200

    3.3-3.6

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
     {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
       },
       {
-        "name": "Martti Tienari",
-        "number": "040-123456",
-        "id": 2
+        name: "Martti Tienari",
+        number: "040-123456",
+        id: 2
       },
       {
-        "name": "Arto Järvinen",
-        "number": "040-123456",
-        "id": 3
+        name: "Arto Järvinen",
+        number: "040-123456",
+        id: 3
       },
       {
-        "name": "Lea Kutvonen",
-        "number": "040-123456",
-        "id": 4
+        name: "Lea Kutvonen",
+        number: "040-123456",
+        id: 4
       }
 ]
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    console.log(id)
+    const person = persons.find(p => p.id === id)
+
+    if ( person ) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+  
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const id = Math.round((Math.random() * 10000))
+    const body = req.body
+    
+    if (body.name === undefined || body.number === undefined) {
+        return res.status(400).json({error: 'name or number missing'})
+    }
+
+    const sameName = persons.find(p => p.name === body.name)
+    if( sameName ){
+        return res.status(400).json({error: 'name must be unique'})
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: id
+    }
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 app.get('/info', (req, res) => {
