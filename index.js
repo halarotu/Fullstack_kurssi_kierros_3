d7543fd12b63f7ac2ca123a269de41f175fc3e01
halarotu <hannes.alarotu@gmail.com>
2018-02-11 22:29:33 +0200

    3.16 fix 3

--- index.js
+++ index.js
@@ -74,9 +74,8 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
     Person
-        .findByIdAndRemove(id)
+        .findByIdAndRemove(req.params.id)
         .then(result => {
             res.status(204).end()
         })
