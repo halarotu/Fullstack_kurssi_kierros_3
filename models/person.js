18acb32cfd0fd9ca3a3cebf9d95d72c690e876ed
halarotu <hannes.alarotu@gmail.com>
2018-02-11 18:21:33 +0200

    3.14 fix 5

--- models/person.js
+++ models/person.js
@@ -1,11 +1,10 @@
-import { Schema } from 'mongoose';
-
 const mongoose = require('mongoose')
 
 const dbuser = 'fullstack_kurssi'
 const dbpassword = process.env.dbpassword || 'xxx'
 const url = `mongodb://${dbuser}:${dbpassword}@ds227858.mlab.com:27858/my-mongo`
 
+const Schema = mongoose.Schema
 mongoose.connect(url)
 mongoose.Promise = global.Promise
 
